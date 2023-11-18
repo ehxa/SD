f9d221c483134ed10b50093c8525dadf99672adb
[GenesisDN] <[2033819@student.uma.pt]>
2023-11-19 00:25:39 +0100

    Token global fixed it

--- client/globals.jsx
+++ client/globals.jsx
@@ -1,9 +1,5 @@
 const Globals = {
-<<<<<<< HEAD
     token: '649V8hvqpJBZVAIesJ7oO0CyRUtB52rQVnaT2myQ',
-=======
-    token: 'PuvAo1bzS2NF2yaCqWXvDPKUy1STNxcMLGM5AWqw',
->>>>>>> 4aacc505e68de91474aa2c274026573d0c41fb80
 };
 
 export default Globals;
